29db7e4a0eb838241905a6f37b93502ad9f49710
asifali-ctrl <asifalinon123@gmail.com>
2025-10-06 08:59:17 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=1443
+let accountNmane="asifali@gmail"
+var accountPassword="asifmehar"
+accountCity="asansol"
+let accountState;
+// accountId=122   this is not allowed
+console.log(accountId);
+accountNmane="ali@gmail.com"
+accountPassword="mehar@123"
+accountCity="panagarh"
+console.table([accountNmane,accountPassword,accountCity, accountState])    // used to print values in tables

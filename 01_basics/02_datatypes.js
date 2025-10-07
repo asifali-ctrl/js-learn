4eb33005fd375c1f1bc2f7c6c25838db441c0d62
asifali-ctrl <asifalinon123@gmail.com>
2025-10-07 08:55:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name="asif ali"
+let age=22
+let isLoggeIn=true
+let state=null
+
+// primitive datatype
+//number=>2 to the power 53
+//bigint=> USED FOR VERY BIG INDIGER
+//STRING=> ALWAYS WRITTEN IN "" OR ''
+//Boolean=> TRUE/FALSE
+//NULL=> STAND ALONE VALUE
+//UNDIFINED=> NOT DEFINED
+//symbol=> unique
+
+
+//object
+
+
+console.log(typeof state);

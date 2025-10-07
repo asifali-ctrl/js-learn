336749561886d80f9ce77edd821e9346c8084a95
asifali-ctrl <asifalinon123@gmail.com>
2025-10-07 09:26:31 +0000

    conversion  operations

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,27 @@
+let score="66abc"
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(typeof(valueInNumber))
+
+//console.log(valueInNumber)
+// "66"=> 66
+// "66abc"=> NAN
+//true=> 1; false=>0
+
+let isLoggeIn="asif"
+let booleanIsLoggeIn=Boolean(isLoggeIn)
+//console.log(booleanIsLoggeIn)
+//console.log(typeof(booleanIsLoggeIn))
+
+// 1=> true; 0=> flase
+// ""=> false
+//"asif"=>true
+
+let someNumber=66
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber));
+
+
+

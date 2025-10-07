4ce49b1c7d959b953a13e49ac430867e037e9de2
asifali-ctrl <asifalinon123@gmail.com>
2025-10-07 09:57:22 +0000

    operation presidensies

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -20,8 +20,23 @@ let booleanIsLoggeIn=Boolean(isLoggeIn)
 
 let someNumber=66
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber));
+//console.log(stringNumber)
+//console.log(typeof(stringNumber));
+
+let value=70
+let NegValue= -value
+//console.log(NegValue)
+
+let str1="hello"
+let str2=" asif"
+let str3 =str1+str2
+//console.log(str3)
+
+//console.log("2"+1);
+//console.log(2+"1");
+//console.log("1"+2+2);
+//console.log(1+2+"2")
+
 
 
 
